724036c5b782c8ca3c0ce1144031f4194384d5bd
Ball-Man <francesco.mistri98@gmail.com>
2017-02-15 11:15:09 +0100

    Working AES First draft of the AES algorithm, using CryptographicEngine from .net There's still no order thoght

--- AES-UWP/MainPage.xaml.cs
+++ AES-UWP/MainPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Security.Cryptography.Core;
+using Windows.Security.Cryptography;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage.Streams;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +28,22 @@ namespace AES_UWP
         public MainPage()
         {
             this.InitializeComponent();
+
+            Random rnd = new Random();
+
+            byte[] key = new byte[64];
+            rnd.NextBytes(key);
+            byte[] data = new byte[3] { 1, 2, 3 };
+
+            SymmetricKeyAlgorithmProvider AES = SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithmNames.AesEcbPkcs7);
+            CryptographicKey cryKey = AES.CreateSymmetricKey(CryptographicBuffer.CreateFromByteArray(key));
+            IBuffer buffData = CryptographicBuffer.CreateFromByteArray(data);
+
+            IBuffer crypted = CryptographicEngine.Encrypt(cryKey, buffData, null);
+
+            IBuffer decrypted = CryptographicEngine.Decrypt(cryKey, crypted, null);
+            byte[] decryptedData = decrypted.ToArray();
+
         }
     }
 }
